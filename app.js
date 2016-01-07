4fb437d3637802bff05fca1695e485b7ef312519
Jason Woods <jwoo@Jasons-Mac-Pro.local>
2016-01-06 22:56:01 -0700

    react starter kit with xpress serve to serve to heroku

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/dist'));
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
